b14a606797628efa058976ef4c9ce245a20d055c
Akylai Otkulbek <akylaiotkulbek@gmail.com>
2024-07-30 22:37:36 +0600

    added values

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Hurra die Welt Geht Unter!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.5;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
